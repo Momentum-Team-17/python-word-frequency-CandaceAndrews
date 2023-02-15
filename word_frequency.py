320a6c2e7d22b88fada7da0ac62ffa24d347bce5
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-02-15 16:56:05 -0500

    Need to format results like README.txt

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,17 +7,61 @@ STOP_WORDS = [
 ]
 
 
+# PUNCTUATION = [",", "?", "!", ".", "'", '"']
+# def remove_punctuation(s):
+#     for punc in PUNCTUATION:
+#         s = s.replace(punc, "")
+#     return s
+
+
+def remove_punctuation(words):
+    stripped_file = words.translate(str.maketrans('', '', string.punctuation))
+    return stripped_file
+
+
+def remove_stop_words(word_list):
+    cleaned_list = []
+    for word in word_list:
+        if word not in STOP_WORDS:
+            cleaned_list.append(word)
+    return cleaned_list
+
+
 def open_file(file):
-    '''Uses 'open' to read a text file'''
+    '''Uses `open` to read a text file'''
     with open(file) as opened_file:
+        # file remains open for the indented lines under here
         read_file = opened_file.read()
-        word_list = read_file.split()
-    print(word_list)
+    # remove punctuation
+    stripped_file = remove_punctuation(read_file).lower()
+    word_list = stripped_file.split()
+    cleaned_list = remove_stop_words(word_list)
+    return cleaned_list
+
+
+def sort_dictionary(dictionary):
+    sorted_word_count_by_frequency = sorted(
+        dictionary.items(), key=lambda x: x[1], reverse=True)
+    return sorted_word_count_by_frequency
+
+
+def formated_results()
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    open_file(file)
+    # Use `open` to read a text file
+    words_to_count = open_file(file)
+    word_count = {}
+    for word in words_to_count:
+        if word in word_count.keys():
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    sorted_word_count = sort_dictionary(word_count)
+    print(sorted_word_count)
+    return sorted_word_count
+    # loop through the list of words, and updated the dictionary to indicate how many of each we have
 
 
 if __name__ == "__main__":
